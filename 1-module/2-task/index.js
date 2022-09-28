f79726f62d8d1851a85fa7a886cec90be0fd5c39
LisaB1994 <79035700491@yandex.ru>
2022-09-28 23:07:10 +0300

    homework-validation

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,14 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+
+  if (name != null && name.length >= 4 && name.indexOf(' ') === -1)
+  return true;
+
+  else {
+    return false;
+  }
+
 }
 
 function sayHello() {
