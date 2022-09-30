440931beaf574cf4c31ad35951e3ac862519929d
LisaB1994 <79035700491@yandex.ru>
2022-09-30 03:27:41 +0300

    factorial-fixes

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,8 +1,10 @@
 function factorial(n) {
-  if (n === 0 || n === 1)
-    return 1;
-  for (i = n - 1; i >= 1; i--) {
-    n *= i;
-  }
-  return n;
+
+  let result = 1;
+
+    while (n) {
+        result *= n--;
+    }
+
+    return result;
 }
