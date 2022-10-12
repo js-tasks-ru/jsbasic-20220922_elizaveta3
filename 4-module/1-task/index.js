b44d343f65af6c0661a87589c571a71ccfaec18f
LisaB1994 <79035700491@yandex.ru>
2022-10-12 21:13:41 +0300

    do-homework-lecture-4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,9 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("UL");
+  for (let item of friends) {
+    let li = document.createElement("LI");
+    li.innerHTML = `${item.firstName} ${item.lastName}`;
+    ul.append(li);
+  }
+  return ul;
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,22 @@
 function highlight(table) {
-  // ваш код...
-}
+  let tbody = table.querySelector('tbody');
+  let trs = tbody.querySelectorAll('tr');
+  
+  for (let i = 0; i < trs.length; i++) {
+  
+  let availableStatus = trs[i].cells[3];
+  
+  availableStatus.dataset.available === 'true' ? trs[i].classList.add("available") : trs[i].classList.add("unavailable")
+  
+  
+  !availableStatus.hasAttribute('data-available') ? trs[i].setAttribute('hidden','') : false
+  
+  let genderStatus = trs[i].cells[2];
+  
+  genderStatus.textContent === "m" ? trs[i].classList.add("male") : trs[i].classList.add("female")
+  
+  let ageStatus = trs[i].cells[1];
+  +ageStatus.innerHTML < 18 ? trs[i].style.textDecoration = "line-through" : false
+  
+  }
+  }
