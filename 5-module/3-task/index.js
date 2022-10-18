432ee7054ae752e69b40178b50fac79829189854
LisaB1994 <79035700491@yandex.ru>
2022-10-19 00:30:00 +0300

    task1-2

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,16 @@
 function initCarousel() {
-  // ваш код...
+  let container = document.querySelector(".container");
+  let arrowRight = container.querySelector(".carousel__arrow_right");
+  let arrowLeft = container.querySelector(".carousel__arrow_left");
+  let window = container.querySelector(".carousel__inner");
+  let card = container.querySelector(".carousel__slide");
+  /* let widthCard = card.offsetWidth; */
+
+  arrowRight.onclick = function () {
+    window.style.transform = "translateX(-600px)";
+  };
+
+  arrowLeft.onclick = function () {
+    window.style.transform = "translateX(100%)";
+  };
 }
